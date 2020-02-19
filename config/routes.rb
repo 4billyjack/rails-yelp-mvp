01ee6781000f3470a1f74c3848a7905d130b8f9a
Bill Jenkins <bjenkins@anewpt.com>
2020-02-19 13:39:16 +0100

    message

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+
+  # get '/restaurants', to: 'restaurants#index'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
+  # get 'restaurants/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
+
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+
 end
